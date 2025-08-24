62a8891b1a34bb2e02debebb33e61e144b578766
Ava Otoadese <ava.otoadese@emory.edu>
2025-08-24 21:50:25 +0000

    Created univariate lm regression

---
+++ R code for Final Project.R
@@ -0,0 +1,36 @@
+install.packages("gtsummary", dependencies = TRUE)
+install.packages("here")
+install.packages("tidyverse")
+ebola <-(here::here("data", "ebola.csv"))
+ebola <- read.csv("data/ebola.csv")
+                  
+#create a table for ebola dataset using gtsummary
+library(gtsummary)
+tbl_summary(
+  ebola,
+  by = Country,
+  include = c(Indicator, Date, value))
+
+#create a regression for ebola dataset using gtsummary
+tbl_uvregression(
+  ebola, 
+  y = value,
+  include = c(Country, Indicator,
+              Date, value),
+  method = lm)
+
+#create a log regression for ebola dataset using gtsummary
+tbl_uvregression(
+  ebola, 
+  y = Country,
+  include = c(Indicator, Date,
+              value,
+  method = glm,
+  method.args = list(family = binomial()),
+  exponentiate = TRUE))
+#FIX this ^^
+
+
+hist(ebola$value)
+
+  
